30fa24f2d27aca4fde53e8cce9df6ff67df7427f
DerThorsten <derthorstenbeier@gmail.com>
2022-08-17 13:00:49 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import panzoom from 'panzoom';
 import flicking from '@egjs/flicking';
-
+import createModule from './pyjs_runtime_browser.js';
 console.log(panzoom);
 console.log(flicking);
 
@@ -10,3 +10,28 @@ var element = document.querySelector('#scene')
 
 // And pass it to panzoom
 panzoom(element)
+
+// (async function(){
+//     console.log("in async main")
+// })()
+
+
+const asyncMain = async () => {
+    console.log("in async main")
+
+    var pyjs = await createModule()
+    globalThis.Module = pyjs
+    pyjs.empackSetStatus = function(status, packageName, downloaded,total){
+        var str = `Downloading data: ${downloaded} / ${total}`
+        console.log(str)
+    }
+    console.log("Download data ...")
+    const { default: load_all }  = await import('./sample_webpack_example.js')
+    console.log("load_all",load_all)
+    await load_all()
+    await pyjs.init()
+
+
+};
+
+asyncMain()
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
 const path = require('path');
 
 module.exports = {
@@ -11,6 +13,18 @@ module.exports = {
       template: './src/index.html',
       filename: 'index.html',
       inject: 'body'
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: 'src/pyjs_runtime_browser.wasm',
+          to: '.'
+        },
+        {
+          from: 'src/*.data',
+          to: "[name][ext]",
+        },
+      ]
     })
   ],
   mode: 'development',
