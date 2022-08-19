9e04ec0cdc128a0474ea8dd0d830a30b3dd8014d
DerThorsten <derthorstenbeier@gmail.com>
2022-08-19 11:50:26 +0200

    wip

--- src/index.js
+++ src/index.js
@@ -1,18 +1,71 @@
 
-import jQuery from 'jquery'
-import panzoom from 'panzoom';
-import flicking from '@egjs/flicking';
+import * as monaco_api from 'monaco-editor/esm/vs/editor/editor.api';
+import * as monaco from 'monaco-editor';
+
+import css from 'xterm/css/xterm.css'
+import { Terminal } from 'xterm';
+import { FitAddon } from 'xterm-addon-fit';
 import {createModule} from './pyjs_runtime_browser.js';
-// console.log(panzoom);
-// console.log(flicking);
 
-var element = document.querySelector('#scene')
-panzoom(element)
 
-import bootstrap from 'bootstrap'
+import css5 from '@fortawesome/fontawesome-free/css/all.min.css'
+
+// import css2 from 'bootstrap/dist/css/bootstrap.min.css';
+// import css3 from 'bootstrap/dist/css/bootstrap.min.css';
+// import css4 from './css/pushbutton.css';
+// import * as bootstrap from 'bootstrap'
+
+// Import our custom CSS
+import './scss/main.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+
+
+
+// The Monaco Editor can be easily created, given an
+// empty container and an options literal.
+// Two members of the literal are "value" and "language".
+// The editor takes the full size of its container.
+(function () {
+    // create div to avoid needing a HtmlWebpackPlugin template
+    const div = document.createElement('div');
+    div.id = 'root1';
+    div.style = 'height:600px; border:1px solid #ccc;';
+    document.body.appendChild(div);
+
+
+    const div2 = document.createElement('div');
+    div2.id = 'xterm-container';
+    div2.style = 'height:400px; border:1px solid #ccc;';
+    document.body.appendChild(div2);
+})();
+
+
+let editor = monaco.editor.create(document.getElementById('editor-container'), {
+    value: `import numpy\nprint(numpy.ones(4,5))`,
+    language: 'python',
+    theme: 'vs-dark',
+    contextmenu: true,
+    automaticLayout: true
+});
+console.log("the editor", editor)
+
+const terminal = new Terminal();
+terminal.open(document.getElementById('terminal-container'));
+const fitAddon = new FitAddon();
+terminal.loadAddon(fitAddon);
+fitAddon.fit();
+
+
+
 
 
 const asyncMain = async () => {
+
+
+
     console.log("in async main")
 
     var pyjs = await createModule()
--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,88 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const webpack = require('webpack');
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 
 const path = require('path');
 
 module.exports = {
-  plugins: [
-    new webpack.ProvidePlugin({
-           $: "jquery",
-           jQuery: "jquery"
-    }),
-    new HtmlWebpackPlugin({
-      hash: true,
-      title: 'Webpack Example App',
-      header: 'Webpack Example Title',
-      metaDesc: 'Webpack Example Description',
-      template: './src/index.html',
-      filename: 'index.html',
-      inject: 'body'
-    }),
-    new CopyPlugin({
-      patterns: [
-        {
-          from: 'src/pyjs_runtime_browser.wasm',
-          to: '.'
-        },
-        {
-          from: 'src/*.data',
-          to: "[name][ext]",
-        },
-      ]
+	module: {
+		rules: [
+      {
+  			test: /\.css$/,
+  			use: [
+  				'style-loader',
+  				'css-loader'
+  			]
+  		},
+      {
+        test: /\.ttf$/,
+        use: ['file-loader']
+      },
+      // bootstrap stuff
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: () => [
+                  require('autoprefixer')
+                ]
+              }
+            }
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
+	},
+	plugins: [
+		new webpack.ProvidePlugin({
+			$: "jquery",
+			jQuery: "jquery"
+		}),
+		new HtmlWebpackPlugin({
+			hash: true,
+			title: 'Webpack Example App',
+			header: 'Webpack Example Title',
+			metaDesc: 'Webpack Example Description',
+			template: './src/index.html',
+			filename: 'index.html',
+			inject: 'body'
+		}),
+		new CopyPlugin({
+			patterns: [{
+				from: 'src/pyjs_runtime_browser.wasm',
+				to: '.'
+			}, {
+				from: 'src/*.data',
+				to: "[name][ext]",
+			}, ]
+		},
+    new MonacoWebpackPlugin({
+      languages: ['typescript', 'javascript', 'css', 'json','python']
     })
-  ],
-  mode: 'development',
-  watch: true, // Enable watch mode
-  target: 'web',
-  output: {
-    clean: true
-  },
-  devServer: {
-    static: './dist',
-    open: true
-  }
+    )
+	],
+	mode: 'development',
+	target: 'web',
+	output: {
+		clean: true,
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.js'
+	},
+	devServer: {
+		static: './dist',
+		open: true
+	}
 };
