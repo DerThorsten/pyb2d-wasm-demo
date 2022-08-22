87b277690b956db1460cad52bd993ca93efd06ec
DerThorsten <DerThorsten@users.noreply.github.com>
2022-08-22 14:17:23 +0000

    deploy: 8816bd595203c60327bafe427294025d7242d67b

---
+++ python/canvas_debug_draw.py
@@ -0,0 +1,132 @@
+import pyjs
+import b2d
+
+
+class HtmlCanvasBatchDebugDraw(b2d.batch_debug_draw_cls(False, False, True)):
+    def __init__(self, canvas, js_canvas_debug_draw, flags=None):
+        super(HtmlCanvasBatchDebugDraw, self).__init__()
+        self.canvas = canvas
+        self.context = canvas.getContext("2d")
+        self.js_canvas_debug_draw = js_canvas_debug_draw
+
+        # what is drawn
+        if True or flags is None:
+            flags = [
+                "shape",
+                "joint",
+                "particle",
+            ]  # ,'aabb','pair','center_of_mass','particle']
+        self.flags = flags
+        self.clear_flags(
+            ["shape", "joint", "aabb", "pair", "center_of_mass", "particle"]
+        )
+        for flag in flags:
+            self.append_flags(flag)
+
+    def _draw_solid_polygons(self, points, sizes, colors):
+
+        js_points = pyjs.buffer_to_js_typed_array(points.ravel())
+        js_sizes = pyjs.buffer_to_js_typed_array(sizes)
+        js_colors = pyjs.buffer_to_js_typed_array(colors.ravel())
+
+        self.js_canvas_debug_draw.draw_polygons(js_points, js_sizes, js_colors, True)
+
+    def _draw_polygons(self, points, sizes, colors):
+
+        js_points = pyjs.buffer_to_js_typed_array(points.ravel())
+        js_sizes = pyjs.buffer_to_js_typed_array(sizes)
+        js_colors = pyjs.buffer_to_js_typed_array(colors.ravel())
+
+        self.js_canvas_debug_draw.draw_polygons(js_points, js_sizes, js_colors, False)
+
+    def _draw_solid_circles(self, centers, radii, axis, colors):
+
+        js_centers = pyjs.buffer_to_js_typed_array(centers.ravel())
+        js_radii = pyjs.buffer_to_js_typed_array(radii)
+        js_axis = pyjs.buffer_to_js_typed_array(axis.ravel())
+        js_colors = pyjs.buffer_to_js_typed_array(colors.ravel())
+
+        self.js_canvas_debug_draw.draw_solid_circles(
+            js_centers, js_radii, js_axis, js_colors
+        )
+
+    def _draw_circles(self, centers, radii, colors):
+        js_centers = pyjs.buffer_to_js_typed_array(centers.ravel())
+        js_radii = pyjs.buffer_to_js_typed_array(radii)
+        js_colors = pyjs.buffer_to_js_typed_array(colors.ravel())
+
+        self.js_canvas_debug_draw.draw_circles(js_centers, js_radii, js_colors)
+
+    def _draw_points(self, centers, sizes, colors):
+        pass
+
+    def _draw_segments(self, points, colors):
+        js_points = pyjs.buffer_to_js_typed_array(points.ravel())
+        js_colors = pyjs.buffer_to_js_typed_array(colors.ravel())
+        self.js_canvas_debug_draw.draw_segments(js_points, js_colors)
+
+    def _draw_particles(self, centers, radius, colors=None):
+        pass
+
+    # non-batch api
+    def draw_solid_circle(self, center, radius, axis, color):
+        # print("draw_solid_circle")
+        center = self.world_to_screen(center)
+        radius = self.world_to_screen_scale(radius)
+        center = numpy.require(center)
+        axis = numpy.require(axis)
+        color = (numpy.require(color) * 255.0).astype("uint8")
+
+        js_center = pyjs.buffer_to_js_typed_array(center.ravel())
+        js_axis = pyjs.buffer_to_js_typed_array(axis.ravel())
+        js_color = pyjs.buffer_to_js_typed_array(color.ravel())
+
+        self.js_canvas_debug_draw.draw_circle_impl(js_center, radius, js_color, js_axis)
+
+    def draw_circle(self, center, radius, color, line_width=1):
+        center = self.world_to_screen(center)
+        radius = self.world_to_screen_scale(radius)
+        center = numpy.require(center)
+        color = (numpy.require(color) * 255.0).astype("uint8")
+
+        js_center = pyjs.buffer_to_js_typed_array(center.ravel())
+        js_color = pyjs.buffer_to_js_typed_array(color.ravel())
+
+        self.js_canvas_debug_draw.draw_circle_impl(js_center, radius, js_color)
+
+    def draw_segment(self, p1, p2, color, line_width=1):
+        p1 = pyjs.buffer_to_js_typed_array(numpy.require(self.world_to_screen(p1)))
+        p2 = pyjs.buffer_to_js_typed_array(numpy.require(self.world_to_screen(p2)))
+        color = (numpy.require(color) * 255.0).astype("uint8")
+        js_color = pyjs.buffer_to_js_typed_array(color.ravel())
+        self.js_canvas_debug_draw.draw_segment(
+            p1, p2, js_color, self.world_to_screen_scale(line_width)
+        )
+
+    def draw_polygon(self, vertices, color, line_width=1):
+        for i, ver in enumerate(vertices):
+            vertices[i] = self.world_to_screen(ver)
+        color = (numpy.require(color) * 255.0).astype("uint8")
+
+        js_points = pyjs.buffer_to_js_typed_array(vertices.ravel())
+        js_color = pyjs.buffer_to_js_typed_array(color.ravel())
+
+        self.js_canvas_debug_draw.draw_poly_impl(
+            int(vertices.shape[0]),
+            js_points,
+            js_color,
+            False,
+            self.world_to_screen_scale(line_width),
+        )
+
+    def draw_solid_polygon(self, vertices, color):
+        for i, ver in enumerate(vertices):
+            vertices[i] = self.world_to_screen(ver)
+        color = (numpy.require(color) * 255.0).astype("uint8")
+
+        js_points = pyjs.buffer_to_js_typed_array(vertices.ravel())
+        js_color = pyjs.buffer_to_js_typed_array(color.ravel())
+
+        self.js_canvas_debug_draw.draw_poly_impl(
+            int(vertices.shape[0]), js_points, js_color, True
+        )
---
+++ python/examples/newtons_cradle.py
@@ -0,0 +1,64 @@
+from b2d.testbed import TestbedBase
+import random
+import numpy
+import b2d
+import math
+import random
+
+
+class NewtonsCradle(TestbedBase):
+
+    name = "newton's cradle"
+
+    def __init__(self, settings=None):
+        super(NewtonsCradle, self).__init__(settings=settings)
+
+        # radius of the circles
+        r = 1.0
+        # length of the rope
+        l = 10.0
+        # how many balls
+        n = 10
+
+        offset = (l + r, 2 * r)
+        dynamic_circles = []
+        static_bodies = []
+        for i in range(n):
+            if i + 1 == n:
+                position = (offset[0] + i * 2 * r + l, offset[1] + l)
+            else:
+                position = (offset[0] + i * 2 * r, offset[1])
+
+            circle = self.world.create_dynamic_body(
+                position=position,
+                fixtures=b2d.fixture_def(
+                    shape=b2d.circle_shape(radius=r * 0.90),
+                    density=1.0,
+                    restitution=1.0,
+                    friction=0.0,
+                ),
+                linear_damping=0.01,
+                angular_damping=1.0,
+                fixed_rotation=True,
+            )
+            dynamic_circles.append(circle)
+
+            static_body = self.world.create_static_body(
+                position=(offset[0] + i * 2 * r, offset[1] + l)
+            )
+
+            self.world.create_distance_joint(
+                static_body,
+                circle,
+                local_anchor_a=(0, 0),
+                local_anchor_b=(0, 0),
+                max_length=l,
+                stiffness=0,
+            )
+
+            static_bodies.append(static_body)
+
+
+async def async_main(context):
+    gui_settings = gui_settings_from_context(context, scale=10, resolution=[500,200])
+    await run_example(NewtonsCradle, gui_settings, context)
